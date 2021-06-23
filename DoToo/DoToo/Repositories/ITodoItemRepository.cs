e70b9885774990073ef6bdba82b664e2cdaf55fe
Jorge Darderes <jdarderes@gmail.com>
2021-06-22 22:00:45 -0400

    Agregar archivos de proyecto.

---
+++ DoToo/DoToo/Repositories/ITodoItemRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DoToo.Models;
+
+namespace DoToo.Repositories
+{
+    public interface ITodoItemRepository
+    {
+        event EventHandler<TodoItem> OnItemAdded;
+        event EventHandler<TodoItem> OnItemUpdate;
+
+        Task<List<TodoItem>> GetItems();
+        Task AddItem(TodoItem item);
+        Task UpdateItem(TodoItem item);
+        Task AddorUpdate(TodoItem item);
+
+    }
+}
